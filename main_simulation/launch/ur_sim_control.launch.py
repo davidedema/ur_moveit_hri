7bec320f0753bd20aeafd52505b1705fd0d1a7b0
davidedema <davide.demartini@studenti.unitn.it>
2024-11-12 14:30:53 +0100

    Started multi-robot simulation

--- main_simulation/launch/ur_sim_control.launch.py
+++ main_simulation/launch/ur_sim_control.launch.py
@@ -46,7 +46,8 @@ from launch_ros.substitutions import FindPackageShare
 def launch_setup(context, *args, **kwargs):
 
     # Initialize Arguments
-    ur_type = LaunchConfiguration("ur_type")
+    ur5_type = LaunchConfiguration("ur5_type")
+    ur3_type = LaunchConfiguration("ur3_type")
     safety_limits = LaunchConfiguration("safety_limits")
     safety_pos_margin = LaunchConfiguration("safety_pos_margin")
     safety_k_position = LaunchConfiguration("safety_k_position")
@@ -54,8 +55,10 @@ def launch_setup(context, *args, **kwargs):
     runtime_config_package = LaunchConfiguration("runtime_config_package")
     controllers_file = LaunchConfiguration("controllers_file")
     description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
-    prefix = LaunchConfiguration("prefix")
+    description_file_ur5 = LaunchConfiguration("description_file_ur5")
+    description_file_ur3 = LaunchConfiguration("description_file_ur3")
+    ur5_prefix = LaunchConfiguration("ur5_prefix")
+    ur3_prefix = LaunchConfiguration("ur3_prefix")
     start_joint_controller = LaunchConfiguration("start_joint_controller")
     initial_joint_controller = LaunchConfiguration("initial_joint_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
@@ -68,13 +71,13 @@ def launch_setup(context, *args, **kwargs):
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
     )
-
-    robot_description_content = Command(
+    
+    robot_ur3_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", description_file]
+                [FindPackageShare(description_package), "urdf", description_file_ur3]
             ),
             " ",
             "safety_limits:=",
@@ -90,10 +93,10 @@ def launch_setup(context, *args, **kwargs):
             "ur",
             " ",
             "ur_type:=",
-            ur_type,
+            ur3_type,
             " ",
             "prefix:=",
-            prefix,
+            ur3_prefix,
             " ",
             "sim_gazebo:=true",
             " ",
@@ -101,13 +104,54 @@ def launch_setup(context, *args, **kwargs):
             initial_joint_controllers,
         ]
     )
-    robot_description = {"robot_description": robot_description_content}
+    
 
-    robot_state_publisher_node = Node(
+    robot_ur5_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", description_file_ur5]
+            ),
+            " ",
+            "safety_limits:=",
+            safety_limits,
+            " ",
+            "safety_pos_margin:=",
+            safety_pos_margin,
+            " ",
+            "safety_k_position:=",
+            safety_k_position,
+            " ",
+            "name:=",
+            "ur",
+            " ",
+            "ur_type:=",
+            ur5_type,
+            " ",
+            "prefix:=",
+            ur5_prefix,
+            " ",
+            "sim_gazebo:=true",
+            " ",
+            "simulation_controllers:=",
+            initial_joint_controllers,
+        ]
+    )
+    robot_ur5_description = {"robot_description": robot_ur5_description_content}
+    robot_ur3_description = {"robot_description": robot_ur3_description_content}
+
+    robot_ur5_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[{"use_sim_time": True}, robot_description],
+        parameters=[{"use_sim_time": True}, robot_ur5_description],
+    )
+    robot_ur3_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[{"use_sim_time": True}, robot_ur3_description],
     )
 
     rviz_node = Node(
@@ -169,7 +213,8 @@ def launch_setup(context, *args, **kwargs):
     )
 
     nodes_to_start = [
-        robot_state_publisher_node,
+        robot_ur5_state_publisher_node,
+        robot_ur3_state_publisher_node,
         joint_state_broadcaster_spawner,
         delay_rviz_after_joint_state_broadcaster_spawner,
         initial_joint_controller_spawner_stopped,
@@ -186,10 +231,18 @@ def generate_launch_description():
     # UR specific arguments
     declared_arguments.append(
         DeclareLaunchArgument(
-            "ur_type",
+            "ur5_type",
             description="Type/series of used UR robot.",
             choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
             default_value="ur5e",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ur3_type",
+            description="Type/series of used UR robot.",
+            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
+            default_value="ur3e",
         )
     )
     declared_arguments.append(
@@ -239,15 +292,31 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_file",
-            default_value="ur.urdf.xacro",
+            "description_file_ur5",
+            default_value="ur5.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "prefix",
-            default_value='""',
+            "description_file_ur3",
+            default_value="ur3.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ur5_prefix",
+            default_value='"robot1"',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ur3_prefix",
+            default_value='"robot2"',
             description="Prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
