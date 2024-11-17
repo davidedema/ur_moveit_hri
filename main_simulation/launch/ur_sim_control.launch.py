fe1877d43dd200381218ce35933354a2568f6805
davidedema <davide.demartini@studenti.unitn.it>
2024-11-17 18:28:53 +0100

    tutto rotto

--- main_simulation/launch/ur_sim_control.launch.py
+++ main_simulation/launch/ur_sim_control.launch.py
@@ -365,7 +365,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "ur5_prefix",
-            default_value='"robot1"',
+            default_value='""',
             description="Prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
@@ -374,7 +374,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "ur3_prefix",
-            default_value='"robot2"',
+            default_value='""',
             description="Prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
